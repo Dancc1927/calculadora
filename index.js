f154d4ed28689f68d50dbdeac444e1d5f801d697
unknown <SENA@ARMAPRCCIFOD576.FormacionSena.RED>
2024-02-26 09:59:00 -0500

    funcion restar

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ function calculadora() {
 function sumar(num1, num2) {
     return num1 + num2;
   }
+function restar(num1, num2) {
+    return num1 - num2;
+  }  
   
   // Función principal del programa
   function calculadora() {
